fdb372d9a9b692927ba515f368fa3363d03be114
Khanh.Nguyen <khanh.nguyen@opower.com>
2015-03-11 20:35:32 -0400

    using async

--- bin/blogger-cli-post.js
+++ bin/blogger-cli-post.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const program = require('commander'),
-    _ = require('lodash'),
-    path = require('path'),
     nconf = require('nconf'),
+    async = require('async'),
     google = require('googleapis'),
     OAuth2Client = google.auth.OAuth2,
-    readPost = require('../lib/read-post'),
-    co = require('co');
+    insertPost = require('../lib/insert-post');
 
 program
     .option('-p, --publish', 'by default a post is draft. Use this option to publish post.')
@@ -38,39 +36,8 @@ let CLIENT_ID = nconf.get('BLOGGER_CLIENT_ID'),
 
 oauth2Client.setCredentials({access_token: accessToken});
 
-insertPostSequentially(files, 0);
-
-// NOTE: we must insert post sequentially, otherwise, we will get Backend error
-// as Google does not allow multiple inserts at the same time.
-function insertPostSequentially(files, idx) {
-    if (idx >= files.length) {
-        return;
-    }
-
-    let file = files[idx],
-        filePath = path.resolve(file);
-
-    readPost(filePath, function(err, postContent) {
-        if (err) {
-            console.log('Fail to read file', filePath);
-            return insertPostSequentially(files, idx + 1);
-        }
-        let opts = _.merge({resource: postContent}, {
-            blogId: blogId,
-            isDraft: isDraft
-        });
-        blogger.posts.insert(opts, function(err, res) {
-            if (err) {
-                if (err.code === 401) {
-                    // TODO: if there's a way to get refresh_token, we can just refresh here.
-                    console.log('Your credential has expired. Use `blogger-cli auth` to reauthorize.');
-                    process.exit(0);
-                }
-                console.log('Fail to insert file', file, 'Error:', err);
-                insertPostSequentially(files, idx + 1);
-            }
-            console.log(file, ': Post inserted successfully! ID =', res.id);
-            insertPostSequentially(files, idx + 1);
-        });
-    });
-}
+// NOTE: we must insert posts sequentially,
+// otherwise, we'll get Backend error.
+async.eachSeries(files, function(file, done) {
+   insertPost(file, blogger, blogId, isDraft, done);
+});
